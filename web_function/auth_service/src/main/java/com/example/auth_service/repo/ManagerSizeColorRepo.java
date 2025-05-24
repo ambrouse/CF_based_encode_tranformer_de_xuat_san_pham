92919371d9593517e5c44aa2aa1a6a2eb4c25c0b
ambrouse <thcihthichich3568@gmail.com>
2025-05-24 20:56:36 +0700

    l

--- web_function/auth_service/src/main/java/com/example/auth_service/repo/ManagerSizeColorRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.auth_service.repo;
-
-import com.example.auth_service.entity.ManagerSizeColorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManagerSizeColorRepo extends JpaRepository<ManagerSizeColorEntity,String> {
-}
--- web_function/product_service/src/main/java/com/example/product_service/repo/ColorRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.product_service.repo;
-
-import com.example.product_service.entity.ColorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ColorRepo extends JpaRepository<ColorEntity,String> {
-}
